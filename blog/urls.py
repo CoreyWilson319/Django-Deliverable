bdfe86453323a27ded7f54c031585cae6f9c3673
Your Name <you@example.com>
2021-01-14 21:50:02 -0500

    finished

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('post/<int:post_id>', views.post_show, name="post_show"),
+    path('post/create', views.PostCreate.as_view(), name="post_create"),
+    path('post/<int:pk>/delete', views.PostDelete.as_view(), name="post_delete"),
+    path('post/<int:pk>/update', views.PostUpdate.as_view(), name="post_update"),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,70 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'First Post',
-        'content': 'Nice blog Corey keep it up',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Agreed',
-        'content': 'I agree with John',
-    },
-    {
-        'author': 'Jimmy Doe',
-        'title': '!!!!',
-        'content': 'Made you look!',
-    },
-]
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
+from .models import Post
+# posts = [
+#     {
+#         'author': 'John Doe',
+#         'title': 'First Post',
+#         'content': 'Nice blog Corey keep it up',
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Agreed',
+#         'content': 'I agree with John',
+#     },
+#     {
+#         'author': 'Jimmy Doe',
+#         'title': '!!!!',
+#         'content': 'Made you look!',
+#     },
+# ]
 
 
 def home(request):
-    content = {
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', content) # The second paramater is a string of the path of the html file
+    posts = Post.objects.all()
+    return render(request, 'posts/index.html', {'posts': posts})
+    # return render(request, 'blog/home.html', content) # The second paramater is a string of the path of the html file
 
 def about(request):
     return render(request, 'blog/about.html') # Third paramater can be added to send things to the html page
+
+def post_show(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts/show.html', {'post': post})
+
+class PostCreate(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = ''
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/')
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/')
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/'
+
+
+def profile(request, username):
+  user = User.objects.get(username=username)
+  posts = Post.objects.filter(user=user)
+  return render(request, 'profile.html', {'username': username, 'posts': posts})
+
+
+
+
 # Create your views here.
